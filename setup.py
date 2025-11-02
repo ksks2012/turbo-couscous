4aafbc29f32289a985a87f4588195853d2195ddc
ksks2012 <st890751999@gmail.com>
2025-11-03 00:33:33 +0800

    build(setup)

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup  # pylint: disable=import-error
+from setuptools import find_packages
+
+setup(name="circular-chromosome-compression",
+		version="0.0.1",
+		description="",
+		packages=find_packages(),
+		install_requires=[
+		],
+		entry_points={
+		},
+		classifiers=[
+				"Development Status :: 3 - Alpha",
+				"Intended Audience :: Developers",
+				"Operating System :: POSIX",
+				"Programming Language :: Python :: 3.12.3",
+		],
+		)
+
+# vim: tabstop=4 shiftwidth=4
